723979c31a21a5e60c800d0bb6f7e645499554b6
Grinexer1 <gavnonaf@mail.ru>
2019-04-26 09:30:55 +0300

    My 2nd commit

---
+++ WpfApp1asds/WpfApp1asds/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WpfApp1asds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(181, 213, 202);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.Show();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            adm adm = new adm();
+            mened men = new mened();
+            skl sk = new skl();
+            user use = new user();
+            SqlConnection sqlcon = new SqlConnection(Properties.Settings.Default.dbConnectionString);
+            sqlcon.Open();
+            SqlCommand command = new SqlCommand("select role from users where login='"+textBox2.Text+"' and pass='"+textBox2.Text+"'", sqlcon);
+            string role = command.ExecuteScalar().ToString();
+            if (role == "admin") { }
+
+        }
+    }
+}
---
+++ WpfApp1asds/WpfApp1asds/Form3.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WpfApp1asds
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(181, 213, 202);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(Properties.Settings.Default.dbConnectionString);
+            string msg = "";
+            int er = 0;
+            //проверка полей на соответсиве
+            if (textBox1.Text == "")
+            {
+                msg += "Введите логин; \n";er++;
+            }
+            if (textBox2.Text == "")
+            {
+                msg += "Введите пароль; \n"; er++;
+            }
+            if (textBox3.Text != textBox2.Text)
+            {
+                msg += "Подтвердите пароль; \n"; er++;
+            }
+            if (textBox4.Text == "")
+            {
+                msg += "Введите имя; \n"; er++;
+            }
+            int forb = 0;
+            int forn = 0;
+            int forc = 0;
+            foreach(char a in textBox2.Text)
+            {
+                if (Char.IsUpper(a))
+                {
+                    forb++;
+                }
+                if (char.IsNumber(a))
+                {
+                    forn++;
+                }
+                if ((a == '!') || (a == '@') || (a == '#') || (a == '$') || (a == '%') || (a == '^'))
+                {
+                    forc++;
+                }
+
+            }
+            if ((textBox2.Text.Length < 6)&&(forb<1)&&(forn<1)&&(forc<1))
+            {
+                msg += "Пароль не соответссвует требованиям; \n"; er++;
+            }
+            if (er > 0)
+            {
+                MessageBox.Show(msg);
+            }
+            else
+            {
+                try
+                {
+                    sqlcon.Open();
+                    SqlCommand command = new SqlCommand("insert into users (login,pass,role,name) values(@login,@pass,@role,@name)", sqlcon);
+                    command.Parameters.AddWithValue("@login", textBox1.Text);
+                    command.Parameters.AddWithValue("@pass", textBox2.Text);
+                    command.Parameters.AddWithValue("@role", "user");
+                    command.Parameters.AddWithValue("@name", textBox4.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Пользователь добавлен");
+                    sqlcon.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Пользователь с таким логином уже существует");
+                }
+            }
+        }
+    }
+}
